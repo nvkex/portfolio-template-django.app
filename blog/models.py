ffac3c6b7bc7a8b1c6d38f323f268fa478c5fca5
nvkex <scrbwrm@gmail.com>
2020-07-07 22:53:26 +0530

    finalized functionality

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pubDate = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) < 97:
+            return self.body
+        else:
+            return self.body[0:97] +"..."
