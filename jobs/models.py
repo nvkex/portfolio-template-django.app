1abdedeec64cb663283509872c603f23c3ff1f55
nvkex <scrbwrm@gmail.com>
2020-07-07 14:12:40 +0530

    Styling and layout added

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+    title = models.CharField(max_length = 100, default = "Project Title")
+    giturl = models.TextField(default = "/")
+    url = models.TextField(default = "/")
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length = 200)
